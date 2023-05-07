30ecdf44f264e16068636ec632e3400e79bd0aff
Anastasia Koroleva <a.koroleva@loftice.ru>
2023-05-07 22:51:04 +0300

    fix: правка названия

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb')
 
 app.use(routes);
 
-routes.use(errors);
-routes.use(handleError);
+app.use(errors);
+app.use(handleError);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
