82cd9175abfdca4b864608d16c1186f783616b03
Anastasia Koroleva <a.koroleva@loftice.ru>
2023-05-07 22:11:53 +0300

    fix: проверка авторизации (в json)

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ const app = express();
 
 const { PORT = 3000 } = process.env;
 const cookieParser = require('cookie-parser');
+const { errors } = require('celebrate');
 const { routes } = require('./routes');
 const auth = require('./middlewares/auth');
+const { handleError } = require('./errors/handleError');
 
 app.all('*', express.json());
 app.use(cookieParser());
@@ -22,6 +24,9 @@ app.use('/', require('./routes/auth'));
 app.use(auth);
 
 app.use(routes);
+
+app.use(errors);
+app.use(handleError);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 
 const routes = express.Router();
-const { errors } = require('celebrate');
 const NotFoundError = require('../errors/NotFoundError');
-const { handleError } = require('../errors/handleError');
 
 routes.use('/users', require('./users'));
 routes.use('/cards', require('./cards'));
@@ -11,8 +9,5 @@ routes.use('/cards', require('./cards'));
 routes.all('*', (req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
 });
-
-routes.use(errors);
-routes.use(handleError);
 
 module.exports = { routes };
